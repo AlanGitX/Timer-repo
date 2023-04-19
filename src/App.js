b32be289c98a5fb162a7991d72755132ea5cbf53
AlanGitX <alanthomas505@gmail.com>
2023-04-19 18:23:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [time, setTime] = useState(59);
+  const [min, setMin] = useState(5);
+  const [hour, Sethour] = useState(0);
+  const [pauser, SetPauser] = useState(true);
+  const [texti, Settext] = useState("Stop");
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (pauser) {
+        setTime(time - 1);
+        if (time === 0) {
+          setTime(59);
+          setMin(min - 1);
+          console.log(min);
+        }
+        console.log(time);
+      }
+      else{
+        console.log('text');
+      }
+    }, 1000);
+
+    if (time === 0 && min == 0) {
+      clearTimeout(timer);
+    }
+  }, [time]);
+
+  const starter = () => {
+    SetPauser(!pauser);
+    if (texti === "Stop") {
+      Settext("Start");
+    } else {
+      Settext("Stop");
+    }
+    console.log(pauser);
+  };
+  const reset = () => {
+    SetPauser(!pauser);
+    setTime(59)
+    setMin(5)
+
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div
+        className="container text-center mt-5 "
+        style={{ backgroundColor: "black" }}
+      >
+        <div style={{ borderBottom: " 1px solid #FFFFFF" }}>
+          <h4 style={{ color: "white" }}>Stopwatch</h4>
+        </div>
+        <div
+          style={{
+            color: "white",
+            display: "flex",
+            marginLeft: "50px",
+            alignItems: "center",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          {" "}
+          <h1>{min}</h1>&nbsp;<p>m</p> &nbsp;&nbsp;&nbsp; <h1>{time}</h1>&nbsp;
+          <p>s</p>
+        </div>
+        <div>
+          <button
+            className="btn m-3"
+            style={{ backgroundColor: "white" }}
+            onClick={() => starter()}
+          >
+            {texti}
+          </button>
+          <button
+            className="btn m-3"
+            style={{ backgroundColor: "white" }}
+            onClick={() => reset()}
+          >
+            Reset
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
